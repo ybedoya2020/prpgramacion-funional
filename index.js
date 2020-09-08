ed6e0384253d837922bf982a76a124545347729c
ybedoya2020 <yebedoya@josegalan.edu.co>
2020-09-08 16:17:56 -0500

    programacion funcional

---
+++ index.js
@@ -0,0 +1,58 @@
+
+
+/* function multiplicar(numero){
+    return numero*5;
+}
+
+let resultado = multiplicar(10);
+ console.log (resultado); 
+ */
+
+ //comportamiento de la funcion
+ /*  
+ let totalcompra = 0;
+ function sumarPrecios(cantidad,valor){
+     return totalcompra = (totalcompra + (cantidad*valor));
+ } 
+ console.log (sumarPrecios(1,100));
+ console.log (sumarPrecios(1,100));
+ console.log (sumarPrecios(1,100));
+ console.log (sumarPrecios(1,100));
+ console.log (sumarPrecios(1,100));
+ */
+ //funcion normal
+
+ /* function multiplicar (numero){
+     return numero*numero
+ }
+ console.log(multiplicar(5)); */
+
+ //funcion composicion funcional 
+  
+ /* function multiplicar (numero){
+    return numero*numero
+ }
+ function suma (num1,num2 ){
+     return multiplicar(num1) + multiplicar (num2)
+ }
+
+ console.log (suma(4,5)) */
+
+ //funciones recursivas
+
+/*  function saludar (){
+     console.log('hola mundo,hola chicos');
+     saludar ();
+ }
+
+ saludar (); */
+
+ //areglo o iniciar elementos de cada arreglo 
+
+ /* const listanumeros = [2,4,6,8,10];
+ const nuevalista = [];
+ for (numero in listanumeros){
+     console.log(listanumeros[numero])
+    nuevalista.push(listanumeros[numero]*2)
+ } 
+console.log(nuevalista) */
